1e7b9fe84bb3410deb6aaeee9c7c551d3f1765bd
{ayushptl1810} <ayushptl1810@gmail.com>
2026-06-17 16:29:42 +0530

    Added new sponsor logo

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -24,6 +24,7 @@ import NinetronLogo from "../assets/Sponsors/Ninetron_logo.png";
 import CUAVLogo from "../assets/Sponsors/CUAV_logo.png";
 import ATCLogo from "../assets/Sponsors/atclogo.png";
 import AltiumLogo from "../assets/Sponsors/altium-designer-software.png";
+import FalconSkyworksLogo from "../assets/Sponsors/Falcon_Skyworks.png";
 
 const Support = () => {
   const sponsorLogos = [
@@ -33,6 +34,12 @@ const Support = () => {
     { src: CUAVLogo, alt: "CUAV", brightness: 5 },
     { src: ATCLogo, alt: "ATC Group", brightness: 5, scale: 2.5 },
     { src: AltiumLogo, alt: "Altium Designer", brightness: 5, scale: 2 },
+    {
+      src: FalconSkyworksLogo,
+      alt: "Falcon Skyworks",
+      brightness: 5,
+      scale: 1.5,
+    },
   ];
 
   const handleContactSubmit = (data) => {
